aa0b520da57ad19fb985fa74e31a7c9bdc1b35be
Justin Zetts <justinzetts@gmail.com>
2022-08-05 17:31:24 -0400

    Pushing SQL Database ready for server integration

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -24,9 +24,24 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
+CREATE TABLE potholes (
+	pothole_id int IDENTITY(1,1) NOT NULL,
+	date_reported date NOT NULL,
+	severity int check (severity between 1 and 3),
+	latitude varchar(25),
+	longitude varchar(25),
+	address varchar(150),
+	city varchar(75),
+	CONSTRAINT PK_pothole PRIMARY KEY (pothole_id)
+)
+
+
+
 -- Populate default data for testing: user and admin with password of 'password'
 -- These values should not be kept when going to Production
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
-GO
 
+INSERT INTO potholes (date_reported, severity, address, city) VALUES ('2022/05/08', 3, '420 Fort Twenty Street', 'Weed City');
+
+GO
