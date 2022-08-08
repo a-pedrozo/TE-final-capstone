a19364977625da645f8338fc921f2dbca35f9c7b
augustine pedrozo <gpedrozo1100@gmail.com>
2022-08-08 09:41:40 -0400

    added Pothole Controller

---
+++ dotnet/Capstone/Controllers/PotholeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.DAO;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+    [Route("pothole")]
+    [ApiController]
+    public class PotholeController : ControllerBase
+    {
+        private readonly IPotholeDAO potholeDAO;
+
+        public PotholeController(IPotholeDAO potholeDAO)
+        {
+            this.potholeDAO = potholeDAO;
+        }
+
+        [HttpGet]
+
+        public ActionResult GetAllPotholes()
+        {
+            List<Pothole> potholes = potholeDAO.GetAllPotholes();
+            if (potholes == null)
+            {
+                return NotFound("Your list is empty or not found");
+            }
+            return Ok(potholes);
+        }
+
+    }
+}
