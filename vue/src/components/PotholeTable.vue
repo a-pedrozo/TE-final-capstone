18efb7e0bacfc4577177593b61326883a0f6f18e
Jessi Austin <jessimasmith.421@gmail.com>
2022-08-08 10:17:36 -0400

    completed front-end connection to API

---
+++ vue/src/components/PotholeTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for='pothole in this.$store.state.potholes' v-bind:key='pothole.id'>
+        {{pothole.id}}, {{pothole.dateReported.substring(0,10)}}, {{pothole.severity}}, {{pothole.latitude}}, {{pothole.longitude}}, {{pothole.address}},{{pothole.city}}
+    </div>
+  </div>
+</template>
+
+<script>
+import PotholeService from '../services/PotholeService.js';
+
+export default {
+    methods: {
+        getListOfPotholes(){
+            PotholeService.getPotholes()
+                .then(response => {
+                    this.$store.commit('LIST_POTHOLES',response.data);
+                })
+        }
+    },
+    created() {
+        this.getListOfPotholes();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <h1>Home</h1>
     <p>Your content goes here. Make it look nice.</p>
+    <pothole-table />
   </div>
 </template>
 
+
 <script>
+import PotholeTable from '../components/PotholeTable.vue';
+
 export default {
+  components: { PotholeTable },
   name: "home"
+  
 };
 </script>
