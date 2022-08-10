5a3c74433d36a569795ae6a5e326af32bb54d783
Justin Zetts <justinzetts@gmail.com>
2022-08-10 15:38:26 -0400

    Pushing changes

--- vue/src/components/PotholeTable.vue
+++ vue/src/components/PotholeTable.vue
@@ -5,12 +5,11 @@
 
     <div v-for='pothole in $store.state.potholes' v-bind:key='pothole.id'>
         <div v-if='pothole.isReviewed || $store.state.user.role=="admin"' class="cards">
-            
         <router-link 
         v-bind:to="{ name: 'PotholeDetails', params: { id: pothole.id } }">
         <p class="id">ID: {{pothole.id}} </p> </router-link>
             <p class="date-reported">Date Reported: {{pothole.reportDate.substring(0,10)}}</p>
-            <p class="severity">Severity: {{pothole.severity}}</p>
+            <p class="reviewed" v-if='$store.state.user.role=="admin"'>Reviewed: {{pothole.isReviewed == true ? 'Yes' : 'No'}}</p>
             <p class="location-lat-and-long">Location (latitude and longitude): {{pothole.latitude}}, {{pothole.longitude}}</p>
             <p class="location-address">Location (address): {{pothole.address}}</p>
             <p class="city">City: {{pothole.city}}</p>
@@ -28,6 +27,7 @@ export default {
             PotholeService.getPotholes()
                 .then(response => {
                     this.$store.commit('LIST_POTHOLES',response.data);
+                    console.log(response.data);
                 })
         }
     },
