33d544ade23e884c78ed0e22af7f93b33a0420e2
augustine pedrozo <gpedrozo1100@gmail.com>
2022-08-12 15:26:13 -0400

    css homepage changes

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,39 +1,65 @@
 <template>
-  <div class="home">
-
-    <header>
+  <div>
+    <header class="home">
       <h1>Home</h1>
     </header>
-    <body>
-      <h1>Hello from home</h1>
-      <div>
-       <map-test />
+    <body class="content">
+      <div class="sweatyholes">
+        <h3>Hot sweaty holes in your area</h3>
+        <div
+          id="potholelist"
+          v-for="pothole in $store.state.potholes"
+          v-bind:key="pothole.id"
+        >
+          <ul>
+            <li v-if="pothole.isReviewed && !pothole.isRepaired">
+              table of potholes {{ pothole.id }} {{ pothole.status }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="elmap">
+        <map-test />
       </div>
     </body>
-
   </div>
 </template>
 
 
 <script>
-import PotholeService from "../services/PotholeService.js"
+import PotholeService from "../services/PotholeService.js";
 
-import MapTest from '../components/MapTest.vue';
+import MapTest from "../components/MapTest.vue";
 
 export default {
   components: { MapTest },
   created() {
-    
-      PotholeService.getPotholes().then((response) => {
-        this.$store.commit("LIST_POTHOLES", response.data);
-      });
-    }
+    PotholeService.getPotholes().then((response) => {
+      this.$store.commit("LIST_POTHOLES", response.data);
+    });
+  },
 };
 </script>
 <style>
-
 body {
   max-width: 100vw;
 }
 
+.elmap {
+  display: flex;
+  justify-content: right;
+}
+
+#potholelist {
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  display: flex;
+  flex-direction: row;
+}
+.sweatyholes {
+  display: flex;
+  flex-direction: column;
+}
 </style>
