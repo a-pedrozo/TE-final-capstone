3141a4821db08f0d79735083e3e98f576660e286
Laura Martinez <laura.elena/martinez1@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[.[B[B[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D>
2022-08-16 15:34:47 -0400

    pulling

--- vue/src/components/PotholeDetails.vue
+++ vue/src/components/PotholeDetails.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
     <div class="deetzhead">
-      <h1 class="location-address">Address: {{ pothole.address }}, {{pothole.city}}</h1>
+      <h1 class="location-address">{{ pothole.address }}, {{pothole.city}}</h1>
     </div>
     <div v-if="pothole" class="details">
-      <!-- <p class="id">ID: {{ pothole.id }}</p> -->
-      <!-- <p class="city">City: {{ pothole.city }}</p> -->
+      
       <div class="reported-reviewed">
-      <p class="date-reported">Date Reported: {{ pothole.reportDate.substring(0, 10) }}</p>
-      <p class="severity">Severity: {{ pothole.severity }}</p>
       <p class="location-lat-and-long">Latitude: {{ pothole.latitude }}</p>
       <p class="location-lat-and-long">Longitude: {{ pothole.longitude }}</p>
-      <!-- <p class="report-date">Report Date: {{pothole.reportDate.substring(0,10)}}</p> -->
+      <p class="date-reported">Date Reported: {{ pothole.reportDate.substring(0, 10) }}</p>
+      <p class="severity" v-if="pothole.severity==0">Severity: N/A</p>
+      <p class="severity" v-else-if="pothole.severity==1">Severity: Low</p>
+      <p class="severity" v-else-if="pothole.severity==2">Severity: Medium</p>
+      <p class="severity" v-else>Severity: High</p>
+      <p class="report-date">Report Date: {{pothole.reportDate.substring(0,10)}}</p>
 
-      <p class="reviewed">
+      <!-- <p class="reviewed">
         Reviewed: {{ pothole.isReviewed == true ? "Yes" : "No" }}
-      </p>
+      </p> 
       <p class="inspected">
         Inspected: {{ pothole.isInspected == true ? "Yes" : "No" }}
-      </p>
+      </p> -->
 
       </div>
       <div class="repaired-inspected">
@@ -32,12 +34,15 @@
       </p>
 
       <p class="repaired">
-        Repaired: {{ pothole.isRepaired == true ? "Yes" : "No" }}
-      </p>
-      <p class="repair-date">
+        Repaired 
+        <i v-if="!pothole.isRepaired && !pothole.repairDate == null" class="fas fa-times-circle"></i>
+        <i v-if="pothole.isRepaired && pothole.repairDate == null" class="fas fa-check-circle"></i>
+        {{pothole.repairDate.substring(0, 10)}}
+      </p> 
+      <!-- <p class="repair-date">
         Repair Date:
         {{ pothole.repairDate ? pothole.repairDate.substring(0, 10) : "None" }}
-      </p>
+      </p> -->
 
       <p class="reviewed">Status: {{ pothole.status }}</p>
       </div>
